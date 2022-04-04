06479eb92bf26182100a862173354552bd03d099
mickacharp <mickaelcharp@gmail.com>
2022-04-04 15:30:59 +0200

    Add movie form now displays custom errors messages

--- src/app/create-movie/create-movie.component.ts
+++ src/app/create-movie/create-movie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CollectionMoviesService } from '../shared/collection-movies.service';
 import { Movie } from '../models/movie';
+import { max } from 'rxjs';
 
 @Component({
   selector: 'app-create-movie',
@@ -10,6 +11,9 @@ import { Movie } from '../models/movie';
 })
 export class CreateMovieComponent implements OnInit {
   newMovie: Movie = new Movie('', '', 0, '', '', '', '');
+  currentYear: number = new Date().getFullYear();
+
+  synopsisMaxLength: number = 0;
 
   audiences: string[] = ['Alone', 'In couple', 'With family', 'With friends'];
 
@@ -18,7 +22,17 @@ export class CreateMovieComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getSynopsisMaxLength();
+  }
+
+  getSynopsisMaxLength(): void {
+    const synopsis = document.querySelector('#synopsis');
+    const maxLengthAttributeValue = synopsis?.getAttribute('maxlength');
+    if (maxLengthAttributeValue) {
+      this.synopsisMaxLength = Number(maxLengthAttributeValue);
+    }
+  }
 
   addMovie() {
     this.service.addMovie(this.newMovie);
@@ -26,5 +40,10 @@ export class CreateMovieComponent implements OnInit {
 
   navigateToCreatedMovie(): void {
     this.router.navigate([`/movies/${this.newMovie.title}`]);
+  }
+
+  onSubmit() {
+    this.addMovie();
+    this.navigateToCreatedMovie();
   }
 }
